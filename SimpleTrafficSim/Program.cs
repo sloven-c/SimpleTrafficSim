3e59aa24c77f35f740c3bc7f9f1be1cd73b03769
Marto <marto@marto.si>
2025-07-11 00:22:48 +0200

    initial commit

---
+++ SimpleTrafficSim/Program.cs
@@ -0,0 +1,19 @@
+﻿using Raylib_cs;
+
+namespace SimpleTrafficSim;
+
+class Program {
+    static void Main(string[] args) {
+        Raylib.InitWindow(800, 600, "Hello World");
+
+        while (!Raylib.WindowShouldClose()) {
+            Raylib.BeginDrawing();
+            
+            Raylib.ClearBackground(Color.Gray);
+            
+            Raylib.DrawText("Hello World", 12, 12, 20, Color.Red);
+            
+            Raylib.EndDrawing();
+        }
+    }
+}
